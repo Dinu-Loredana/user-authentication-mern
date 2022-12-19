445bcd77523bfb40004424bcec9b09c8599773d6
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-12-19 17:52:23 +0000

    create register form in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,51 @@
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:1337/api/register", {
+      // define route for this endpoint in server; what response to send to client when a req has been made to this endpoint
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    console.log("data, response from server", data);
+  };
   return (
     <div>
       <h1>Register</h1>
-      <form action="">
-        <input type="text" placeholder="Name" />
+      <form onSubmit={registerUser}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <br></br>
-        <input type="email" placeholder="Email" />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br></br>
-        <input type="password" placeholder="Password" />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br></br>
         <button type="submit">Register</button>
       </form>
