e60064e8c595aa87b43849a1309cdf264aad456d
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-12-20 12:01:33 +0000

    set token into local storage, get quote based on jwt token in Dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,32 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import jwt from "jsonwebtoken";
+import { useNavigate } from "react-router-dom";
 function Dashboard() {
+  const navigate = useNavigate();
+  const populateData = async () => {
+    const req = await fetch("http://localhost:1333/api/quote", {
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    });
+    const quote = req.json();
+    console.log("quote", quote);
+  };
+  useEffect(() => {
+    //get the token from local storage
+    const token = localStorage.getItem("token");
+    // check the token with decoded valie
+    if (token) {
+      const user = jwt.decode(token);
+      if (!user) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      } else {
+        // if user exists populate the data
+        populateData();
+      }
+    }
+  }, [navigate]);
   return <div>Dashboard</div>;
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -21,6 +21,7 @@ function Login() {
     });
     const data = await response.json();
     if (data.user) {
+      localStorage.setItem("token", data.user);
       alert("Login successfully");
       navigate("/dashboard");
     } else {
