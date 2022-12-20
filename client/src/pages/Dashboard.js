13af66a3b38a1ad45e038d8d828307a525dbf7eb
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-12-20 16:56:35 +0000

    create form for quote and send data to server

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,22 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // import jwt from "jsonwebtoken";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+  const [tempQuote, setTempQuote] = useState("");
+  const [quote, setQuote] = useState("");
   const navigate = useNavigate();
+
+  const saveQuoteHandler = async (e) => {
+    e.preventDefault();
+    const req = await fetch("http://localhost:1333/api/quote", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        quote: tempQuote,
+      }),
+    });
+
+    const data = await req.json();
+    if (data.status === "ok") {
+      setQuote(tempQuote);
+      setTempQuote("");
+    } else {
+      alert(data.error);
+    }
+  };
+
   const populateData = async () => {
     const req = await fetch("http://localhost:1333/api/quote", {
       headers: {
         "x-access-token": localStorage.getItem("token"),
       },
     });
+
+    const data = await req.json();
     console.log("req", req);
-    const quote = req.json();
-    console.log("quote", quote);
+    console.log("data", data);
+
+    if (data.status === "ok") {
+      setQuote(data.quote);
+    } else {
+      alert(data.error);
+    }
   };
+
   useEffect(() => {
-    // get the token from local storage
+    // verify if the user is logedin, get the token from local storage
     const token = localStorage.getItem("token");
     // decode token
     if (token) {
@@ -31,7 +64,21 @@ function Dashboard() {
       }
     }
   }, [navigate]);
-  return <div>Dashboard</div>;
+
+  return (
+    <div>
+      <form onSubmit={saveQuoteHandler}>
+        <input
+          type="text"
+          placeholder="your favorite quote"
+          value={tempQuote}
+          onChange={(e) => setTempQuote(e.target.value)}
+        />
+        <input type="submit" value="Save" />
+      </form>
+      <p>Your quote: {quote || "No quote found. Add a quote."}</p>
+    </div>
+  );
 }
 
 export default Dashboard;
